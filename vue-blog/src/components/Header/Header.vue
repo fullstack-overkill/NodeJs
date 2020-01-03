ef09cb999c687083f518ffce9e1d9fea29506d84
christopher <christophermms.ctt@gmail.com>
2020-01-03 18:28:48 -0300

    555

---
+++ vue-blog/src/components/Header/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <header class="Header_Principal row">
+      <div class="col-sm">
+        <img class="Header_logo" :src="LogoJavaSript" alt="Logo java script">
+      </div>
+      <div class="col-sm dasd">
+          <ul class="Header_Paginas">
+              <li>Home</li>
+              <li>Lore</li>
+              <li>Lore</li>
+              <li>Lore</li>
+          </ul>
+          <div :class="PaginaAtual2()">dasdas</div>
+      </div>
+      <div class="col-">One of three columns</div>
+    </header>
+  </div>
+</template>
+
+<script>
+import LogoJavaSript from "@/assets/img/LogoJavaSript.png"
+export default {
+    name: "HelloWorld",
+    data:  () => {
+        return {
+            LogoJavaSript: LogoJavaSript,
+            PaginaAtual: 1,
+
+        }
+    },
+    props: {
+        msg: String
+    },
+    methods: {
+        PaginaAtual2(){
+            return "MenuDestaque"
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./Header.stylus" lang="stylus"></style>
